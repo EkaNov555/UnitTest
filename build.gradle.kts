2424aa8ed2b934366f665e27eb442f51e3a4cfde
Ekaterina <gaurung505@gmail.com>
2023-12-21 20:18:32 +0300

    testng

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,12 +10,12 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testImplementation ("org.testng:testng:7.1.0")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation ("junit:junit:4.13")
 }
 
 tasks.test {
     useJUnitPlatform()
-    useTestNG()
+    useJUnit()
 }
